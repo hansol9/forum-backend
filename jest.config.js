277ce4d5a95a0caab1c66d7b9db122d62750143d
hansol9 <hansol9@gmail.com>
2026-04-26 15:55:07 +0900

    SCRUM-5 Initialize TypeScript project with Express and TypeORM

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: { branches: 80, functions: 80, lines: 80, statements: 80 },
+  },
+};
